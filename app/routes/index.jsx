959ce7f272980cc5a3ab055db2a7bd6acf2cb036
Pranav <pranav.techiegeek@gmail.com>
2023-03-28 21:40:26 +0530

    first commit

---
+++ app/routes/index.jsx
@@ -0,0 +1,15 @@
+export default function Index() {
+	return (
+		<div
+			style={{
+				fontFamily: "system-ui, sans-serif",
+				lineHeight: "1.4",
+				textAlign: "center",
+				marginBottom: "0px",
+			}}
+		>
+			<h1 style={{ marginBottom: "0px" }}>Remix Playground</h1>
+			<p style={{ marginTop: "0px" }}>@codedamn</p>
+		</div>
+	);
+}
---
+++ remix.config.js
@@ -0,0 +1,9 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+process.env.HOST = "0.0.0.0";
+module.exports = {
+	ignoredRouteFiles: ["**/.*"],
+	// appDirectory: "app",
+	// assetsBuildDirectory: "public/build",
+	// serverBuildPath: "build/index.js",
+	// publicPath: "/build/",
+};
